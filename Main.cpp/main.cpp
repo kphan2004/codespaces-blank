4e59cb37757db0a9af9711a0c859fa96fc341b1f
kphan2004 <143112315+kphan2004@users.noreply.github.com>
2023-09-12 16:33:32 +0000

    first commit

---
+++ Main.cpp/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+{
+    int counter(1);
+    while(counter !=11)
+ 
+    std::cout << counter << "   ";
+    ++counter
+    {
+        std::cout << std::endl;
+        counter=0;
+    }
+
+}
+
+return 0;
+}
