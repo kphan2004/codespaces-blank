792ec341a144db7056bd91337d5d43c6b69c2911
kphan2004 <143112315+kphan2004@users.noreply.github.com>
2025-04-14 04:26:52 +0000

    check

---
+++ Carwash/main.cpp
@@ -0,0 +1,82 @@
+// main.cpp
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <iomanip>
+#include "carWash.h"
+
+using namespace std;
+
+const int SIMULATION_END_TIME = 540;
+
+int main() {
+    ifstream input("arrival_time.txt");
+    if (!input) {
+        cerr << "ERROR opening arrival_time.txt\n";
+        return 1;
+    }
+
+    queue<CarWash> carQueue;
+    int carNumber = 1;
+    int time;
+
+    while (input >> time) {
+        if (time <= SIMULATION_END_TIME) {
+            carQueue.push(CarWash(carNumber++, time));
+        } else {
+            cout << carNumber++ << " Car arrived after cloasing time and was not served.\n";
+        }
+        
+    }
+
+    cout << "Opening Time: 8:00 AM (0 minutes)\n";
+    cout << "Closing Time: 5:00 PM (" << SIMULATION_END_TIME << " minutes)\n";
+    cout << "Start of Simulation\n\n";
+
+    cout << left << setw(15) << "Car number" << setw(15) << "Arrival Time"
+         << setw(15) << "Start Time" << setw(20) << "Departure Time"
+         << setw(12) << "Wait Time" << setw(12) << "Total Time" << endl;
+    cout << "----------------------------------------------------------------------------------------" << endl;
+    
+    
+    int currentTime = 0;
+    int totalWait = 0;
+    int totalCharge = 0;
+    int servedCars = 0;
+    int totalCarWashUseTime =0;
+
+    while (!carQueue.empty()) {
+        CarWash car = carQueue.front();
+        carQueue.pop();
+
+        int arrival = car.getArrivalTime();
+        int startTime = max(arrival, currentTime);
+        car.setStartTime(startTime);
+        car.calculateTimes();
+
+        currentTime = car.getDepartureTime();
+        totalWait += car.getWaitTime();
+        totalCharge += 3;
+        servedCars++;
+
+        cout << left << setw(15) << car.getCarNumber()
+             << setw(15) << car.getArrivalTime()
+             << setw(15) << car.getStartTime()
+             << setw(20) << car.getDepartureTime()
+             << setw(12) << car.getWaitTime()
+             << setw(12) << car.getTotalTime() << endl;
+        }
+    
+    cout << "\nEnd of Simulation\n\n";
+    cout << "Statistics:\n";
+    cout << "Total car wash use time: ";
+    cout << "Total Wait Time: " << totalWait << " minutes\n";
+
+    int averageMin = servedCars > 0 ? totalWait / servedCars : 0;
+    int averageSec = servedCars > 0 ? (totalWait * 60 / servedCars) % 60 : 0;
+    cout << "Average Wait Time: " << averageMin << " minutes and " << averageSec << " seconds\n";
+    cout << fixed << setprecision(2);
+    cout << "Percentage of time car was was in use: " << (totalCharge * 100.0 / SIMULATION_END_TIME) << "%\n";
+
+    return 0;
+}
