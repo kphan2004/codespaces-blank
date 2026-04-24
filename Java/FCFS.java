5a8650562962f1c6831a9ed5236f832d35893692
kphan2004 <143112315+kphan2004@users.noreply.github.com>
2026-04-24 08:30:48 +0000

    BANKING

--- Java/FCFS.java
+++ Java/FCFS.java
@@ -2,36 +2,43 @@
  * FCFS scheduling algorithm.
  */
  
-import java.util.*;
+import java.util.*; // Import Java utility classes (List, ArrayList, etc)
 
 public class FCFS implements Algorithm
 {
+    // List that stores all tasks to be scheduled
     private List<Task> queue;
+    // The task currently being executed
     private Task currentTask;
 
+    // Total number of tasks 
     private int tasksRun;
 
     public FCFS(List<Task> queue) {
-        this.queue = queue;
-
+        this.queue = queue; // Assign the passed-in task list to local queue
+        // Store initial number of tasks
         tasksRun = queue.size();
     }
 
     public void schedule() {
+        // Print algorithm name
         System.out.println("FCFS Scheduling \n");
 
+        // Continue until all tasks are processed
         while (!queue.isEmpty()) {
             currentTask = pickNextTask();
             
+            // Run for the full duration of its burst
             CPU.run(currentTask, currentTask.getBurst());
 
-            // remove the task
+            // Task is finished, remove it
             queue.remove(currentTask);
         }
     }
 
     //IMPLEMENT HOW YOU PICK THE NEXT TASK USING FCFS HERE
     public Task pickNextTask() {
-
+        // Returns first task in the list
+        return queue.get(0);
     }
 }
--- Java/Priority.java
+++ Java/Priority.java
@@ -10,5 +10,47 @@ import java.util.*;
 //TAKE A LOOK AT PRIORITYRR.JAVA TO SEE WHAT YOU CAN REUSE HERE
 public class Priority implements Algorithm
 {
+    // List of tasks
+    private List<Task> queue;
+    // Keeps track of current task being executed
+    private Task currentTask;
 
+    public Priority(List<Task> queue) {
+        this.queue = queue; // Initialize queue
+    }
+
+    // Scheduling func.
+    public void schedule() {
+        // Print title
+        System.out.println("Priority Scheduling \n");
+
+        // Continues until all tasks are completed
+        while (!queue.isEmpty()) {
+            // Selects next task based on highest priority
+            currentTask = pickNextTask();
+            // Run task completely
+            CPU.run(currentTask, currentTask.getBurst());
+            // Task is finished, remove it
+            queue.remove(currentTask);
+        }
+    }
+
+    // Selects task with highest priority value
+    public Task pickNextTask() {
+        // Assume first task is highest
+        int highestIndex = 0;
+        // Store its priority
+        int highestPriority = queue.get(0).getPriority();
+
+        // Loop through all tasks to find highest priority
+        for (int i = 1; i < queue.size(); i++) {
+            // If current task has higher priority
+            if (queue.get(i).getPriority() > highestPriority) {
+                highestIndex = i;   // Update index
+                highestPriority = queue.get(i).getPriority();   // Update value
+            }
+        }
+        // Returns task with highest priority
+        return queue.get(highestIndex);
+    }
 }
